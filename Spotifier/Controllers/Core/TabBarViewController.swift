6ecbad37e99dce9dfb8771f80e61bff1b46326be
Edu Caubilla <educaubilla@gmail.com>
2023-07-25 17:18:03 +0200

    Part 3 -> Auth logic

--- Spotifier/Controllers/Core/TabBarViewController.swift
+++ Spotifier/Controllers/Core/TabBarViewController.swift
@@ -28,6 +28,8 @@ class TabBarViewController: UITabBarController {
         let nav2 = UINavigationController(rootViewController: vc2)
         let nav3 = UINavigationController(rootViewController: vc3)
         
+        
+        
         nav1.tabBarItem = UITabBarItem(title: "Browse", image: UIImage(systemName: "house"), tag: 1)
         nav1.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
         nav1.tabBarItem = UITabBarItem(title: "Library", image: UIImage(systemName: "music.note.list"), tag: 1)
--- Spotifier/Managers/AuthManager.swift
+++ Spotifier/Managers/AuthManager.swift
@@ -14,6 +14,8 @@ final class AuthManager{
         static let clientID = "241b109723d14a4490149306e8b7f2ec"
         static let clientSecret = "508f30d334ca43f78e40d608a2a461c8"
         static let tokenAPIURL = "https://accounts.spotify.com/api/token"
+        static let redirectURI = "https://educaubilla.netlify.app"
+        static let scopes = "user-read-private%20playlist-modify-public%20playlist-read-private%20playlist-modify-private%20user-library-read%20user-library-modify%20user-read-email%20streaming"
 
     }
     
@@ -21,32 +23,38 @@ final class AuthManager{
     
     private init(){}
     
-    var isSignedIn: Bool{
-        return false
-    }
-    
     public var signInURL: URL?{
-        let scope = "user-read-private"
+        
         let baseURI = "https://accounts.spotify.com/authorize"
-        let redirectURI = "https://educaubilla.netlify.app"
-        let string = "\(baseURI)?response_type=code&client_id=\(Constants.clientID)&scope=\(scope)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        let string = "\(baseURI)?response_type=code&client_id=\(Constants.clientID)&scope=\(Constants.scopes)&redirect_uri=\(Constants.redirectURI)&show_dialog=TRUE"
         return URL(string: string)
     }
     
+    var isSignedIn: Bool{
+        return accessToken != nil
+    }
+    
+    
     private var accessToken: String?{
-        return nil
+        return UserDefaults.standard.string(forKey: "access_token")
     }
     
     private var refreshToken: String?{
-        return nil
+        return UserDefaults.standard.string(forKey: "refresh_token")
     }
     
     private var tokenExpirationDate: Date? {
-        return nil
+        return UserDefaults.standard.object(forKey: "expiration_date") as? Date
     }
     
     private var shouldRefreshToken: Bool{
-        return false
+        guard let expirationDate = tokenExpirationDate else{
+            return false
+        }
+        
+        let currentDate = Date()
+        let tokenExpirationOffset: TimeInterval = 300
+        return currentDate.addingTimeInterval(tokenExpirationOffset) >= expirationDate
     }
     
     public func exchangeCodeForToken(code: String, completion: @escaping ((Bool) -> Void)){
@@ -59,7 +67,7 @@ final class AuthManager{
         components.queryItems = [
             URLQueryItem(name: "grant_type", value: "authorization_code"),
             URLQueryItem(name: "code", value: code),
-            URLQueryItem(name: "redirect_uri", value: "https://educaubilla.netlify.app"),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
         ]
         
         var request = URLRequest(url: url)
@@ -79,7 +87,7 @@ final class AuthManager{
         request.setValue("Basic \(base64String)",
                          forHTTPHeaderField: "Authorization")
         
-        let task = URLSession.shared.dataTask(with: request){data, _, error in
+        let task = URLSession.shared.dataTask(with: request){ [weak self] data, _, error in
             guard let data = data,
                   error == nil else{
                 completion(false)
@@ -87,11 +95,9 @@ final class AuthManager{
             }
             
             do {
-                let json = try JSONSerialization.jsonObject(
-                    with: data,
-                    options: .allowFragments)
-                
-                print("SUCCESS -> \(json)")
+                let result = try JSONDecoder().decode(AuthResponse.self, from: data)
+                print("Token successfully refreshed")
+                self?.cacheToken(result: result)
                 completion(true)
             }
             catch {
@@ -101,12 +107,71 @@ final class AuthManager{
         task.resume()
     }
     
-    public func refreshAccessToken(){
+    public func refreshAccessToken(completion: @escaping (Bool) -> Void){
+        guard shouldRefreshToken else{
+            completion(true)
+            return
+        }
         
+        guard let refreshToken = self.refreshToken else{
+            return
+        }
+        
+        //Refresh token
+        guard let url = URL(string: Constants.tokenAPIURL) else{
+            return
+        }
+        
+        var components = URLComponents()
+        components.queryItems = [
+            URLQueryItem(name: "grant_type", value: "refresh_token"),
+            URLQueryItem(name: "refresh_token", value: refreshToken),
+        ]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded",
+                         forHTTPHeaderField: "Content-Type")
+        request.httpBody = components.query?.data(using: .utf8)
+        
+        let basicToken = Constants.clientID + ":" + Constants.clientSecret
+        let basicTokenData = basicToken.data(using: .utf8)
+        guard let base64String = basicTokenData?.base64EncodedString() else {
+            print("Fail to get base64")
+            completion(false)
+            return
+        }
+        
+        request.setValue("Basic \(base64String)",
+                         forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request){ [weak self] data, _, error in
+            guard let data = data,
+                  error == nil else{
+                completion(false)
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(AuthResponse.self, from: data)
+                self?.cacheToken(result: result)
+                print("Successfully refreshed")
+                completion(true)
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
     }
     
-    private func cacheToken(){
+    private func cacheToken(result: AuthResponse){
+        UserDefaults.standard.setValue(result.access_token, forKey: "access_token")
         
+        if let refresh_token = result.refresh_token {
+            UserDefaults.standard.setValue(refresh_token, forKey: "refresh_token")
+        }
+
+        UserDefaults.standard.setValue(Date().addingTimeInterval(TimeInterval(result.expires_in)), forKey: "expiration_date")
     }
-    
 }
