9d52ba91744d8ece3970ca80809ed77fae817579
Edu Caubilla <educaubilla@gmail.com>
2023-08-09 12:11:21 +0200

    Part 13 - Add TitleHeaders & AlbumView logic

--- Spotifier/Controllers/Other/AlbumViewController.swift
+++ Spotifier/Controllers/Other/AlbumViewController.swift
@@ -7,8 +7,47 @@
 
 import UIKit
 
-class AlbumViewController: UIViewController {
+class AlbumViewController: UIViewController{
 
+    private let collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewCompositionalLayout(sectionProvider: { _, _ -> NSCollectionLayoutSection? in
+            let item = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .fractionalHeight(1.0)
+                )
+            )
+            item.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 2, bottom: 1, trailing: 2)
+            
+            //VerticalGroup
+            let group = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .absolute(60)
+                ),
+                subitem: item,
+                count: 1
+            )
+            
+            //Section
+            let section = NSCollectionLayoutSection(group: group)
+            section.boundarySupplementaryItems = [
+                NSCollectionLayoutBoundarySupplementaryItem(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .fractionalWidth(1.0),
+                        heightDimension: .fractionalWidth(0.8)),
+                    elementKind: UICollectionView.elementKindSectionHeader,
+                    alignment: .top
+                )
+            ]
+            
+            return section
+        })
+    )
+
+    private var viewModels = [AlbumCollectionViewCellViewModel]()
+    
     private let album: Album
     
     init(album: Album){
@@ -25,15 +64,120 @@ class AlbumViewController: UIViewController {
         title = album.name
         view.backgroundColor = .systemBackground
         
-        APICaller.shared.getAlbumDetails(for: album){ result in
+        view.addSubview(collectionView)
+        collectionView.register(
+            AlbumTrackCollectionViewCell.self,
+            forCellWithReuseIdentifier: AlbumTrackCollectionViewCell.identifier
+        )
+        
+        collectionView.register(
+            PlaylistHeaderCollectionReusableView.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: PlaylistHeaderCollectionReusableView.identifier)
+        
+        collectionView.backgroundColor = .systemBackground
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        APICaller.shared.getAlbumDetails(for: album){ [weak self] result in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let model):
+                    self?.viewModels = model.tracks.items.compactMap({
+                        AlbumCollectionViewCellViewModel(
+                            name: $0.name,
+                            artistName: $0.artists.first?.name ?? "Unknown"
+                        )
+                    })
+                    self?.collectionView.reloadData()
                     break
                 case .failure(let error):
+                    print(error)
                     break
                 }
             }
         }
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .action,
+            target: self,
+            action: #selector(didTapShare)
+        )
+    }
+    
+    @objc private func didTapShare(){
+        guard let url = URL(string: album.href) else {
+            return
+        }
+        print(album.href)
+        let vc = UIActivityViewController(
+            activityItems: [url],
+            applicationActivities: []
+        )
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
     }
 }
+
+
+extension AlbumViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModels.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier:  AlbumTrackCollectionViewCell.identifier,
+            for: indexPath
+        ) as? AlbumTrackCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.configure(with: viewModels[indexPath.row])
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard let header = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: PlaylistHeaderCollectionReusableView.identifier,
+            for: indexPath
+        ) as? PlaylistHeaderCollectionReusableView,
+        kind == UICollectionView.elementKindSectionHeader else {
+            return UICollectionReusableView()
+        }
+        let headerViewModel = PlaylistHeaderViewViewModel(
+            name: album.name,
+            ownerName: album.artists.first?.name ?? "Unknown",
+            //description: album.artists.description,
+            description: "Released: \(String.formattedDate(string: album.release_date))",
+            artworkURL: URL(string: album.images.first?.url ?? ""))
+        header.configure(with: headerViewModel)
+        header.delegate = self
+        return header
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        //Play song
+    }
+}
+
+extension AlbumViewController: PlaylistHeaderCollectionReusableViewDelegate {
+    func playlistHeaderCollectionReusableViewDidTapPlayAll(_ header: PlaylistHeaderCollectionReusableView) {
+        // Start playlist queue
+        print("Play all")
+        
+    }
+}
+
+
+
---
+++ Spotifier/ViewModels/AlbumCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumCollectionViewCellViewModel.swift
+//  Spotifier
+//
+//  Created by Edu Caubilla on 9/8/23.
+//
+
+import Foundation
+
+struct AlbumCollectionViewCellViewModel{
+    let name: String
+    let artistName: String
+}
